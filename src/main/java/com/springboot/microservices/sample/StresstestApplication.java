12a7d907f06eabe456d68b1687057ef7402ee220
happycloudpak <happycloudpak@gmail.com>
2021-04-04 16:15:34 +0900

    change main class

--- src/main/java/com/springboot/microservices/sample/StresstestApplication.java
+++ src/main/java/com/springboot/microservices/sample/StresstestApplication.java
@@ -2,13 +2,12 @@ package com.springboot.microservices.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class StressTestApplication {
+public class StresstestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StressTestApplication.class, args);
+		SpringApplication.run(StresstestApplication.class, args);
 	}
 
 }
